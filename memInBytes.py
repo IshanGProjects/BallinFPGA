43c60341863d32adee11a1985e8cd449754439e5
Ishan Gohil <ishan10.gohil@gmail.com>
2023-04-27 11:28:22 -0600

    Update memInBytes.py

--- memInBytes.py
+++ memInBytes.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 def get_file_size(file_path):
     return os.path.getsize(file_path)
@@ -7,3 +8,4 @@ if __name__ == "__main__":
     file_path = "4g.hex"
     file_size = get_file_size(file_path)
     print(f"File size: {file_size} bytes")
+    print(math.ceil(math.log2(file_size)))
