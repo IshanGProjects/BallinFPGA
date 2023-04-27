0017c2fb93c3214e0b6ee34b528084653c60000f
Ishan Gohil <ishan10.gohil@gmail.com>
2023-04-27 11:14:13 -0600

    Add files via upload
    This program allows users to check the size of the program in bytes

---
+++ memInBytes.py
@@ -0,0 +1,9 @@
+import os
+
+def get_file_size(file_path):
+    return os.path.getsize(file_path)
+
+if __name__ == "__main__":
+    file_path = "4g.hex"
+    file_size = get_file_size(file_path)
+    print(f"File size: {file_size} bytes")
